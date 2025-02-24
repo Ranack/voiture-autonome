963e3a6cdd0c92eefbf561d4e9218b73143b64e5
KvN Roualec <kevin.roualec@hotmail.fr>
2025-02-24 16:39:11 +0100

    Update main.py

--- API/main.py
+++ API/main.py
@@ -35,7 +35,7 @@ app = FastAPI()
 DIRS = {
     "images": "/app/Images/Photos",  # Mise à jour du chemin pour correspondre à votre conteneur Docker
     "masks": "/app/Images/Mask",     # Mise à jour du chemin pour correspondre à votre conteneur Docker
-    "model": "/app/Model/efficientnet_fpn_model_best_iou_diceloss.keras"  # Assurez-vous que ce chemin est correct
+    "model": os.path.join(os.path.dirname(__file__), '..', 'Model', 'efficientnet_fpn_model_best_iou_diceloss.keras')  # Chemin relatif pour le modèle
 }
 
 # Vérifier si le modèle existe
